3c77916060c7450ceb325fbf12f0d877a8927a41
Jason Mueller <jmueller@healthfirst.org>
2023-02-09 14:56:04 -0500

    Updated output

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "instance_info" {
   value       = module.ec2
   description = "The instance info"
 }
+
+output "instance_ip" {
+  value       = module.ec2.private_ip
+  description = "The instance ip"
+}
