220da1af7804429f02f8a21b980c52dc840b3001
jmueller-hf <122904141+jmueller-hf@users.noreply.github.com>
2023-02-26 21:26:07 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,23 +15,38 @@ provider "aws" {
   }
 }
 
+data "aws_ami" "ami" {
+  most_recent = true
+  owners = [lookup(var.ami_filters, var.os_platform).owner]
+
+  filter {
+    name   = "name"
+    values = [lookup(var.ami_filters, var.os_platform).filter]
+  }
+}
+
+resource "random_shuffle" "subnet" {
+  input        = lookup(lookup(var.subnets, var.environment),var.subnet_type)
+  result_count = 1
+}
+
 module "ec2" {
   source  = "app.terraform.io/healthfirst/EC2/aws"
   version = "1.5.0"
-  ami                    = var.ami
+  ami                    = data.aws_ami.ami.id
   instance_type          = var.instance_type
-  subnet_ids             = var.subnet_ids
+  subnet_ids             = random_shuffle.subnet.result
   key_name               = var.key_name
   user_data              = var.user_data
-  instance_profile       = var.instance_profile
+  instance_profile       = "HF-EC2-SSMRole"
   security_groups        = var.security_groups
   instance_name          = var.instance_name
 }
 
-module "bluecat" {
-  source  = "app.terraform.io/healthfirst/bluecat/cln"
-  version = "1.13.0"
-  hostname = var.instance_name
-  password = var.bc_password
-  value    = module.ec2.instance_ip
-}
+#module "bluecat" {
+#  source  = "app.terraform.io/healthfirst/bluecat/cln"
+#  version = "1.13.0"
+#  hostname = var.instance_name
+#  password = var.bc_password
+#  value    = module.ec2.instance_ip
+#}
