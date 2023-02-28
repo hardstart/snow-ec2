e7a849f6a52b6e903e7c1703401e151f97fd525e
Jason Mueller <jmueller@healthfirst.org>
2023-02-27 20:45:41 -0500

    Updated template

--- variables.tf
+++ variables.tf
@@ -1,8 +1,3 @@
-variable "default_tags" {
-  type        = map
-  description = "A map of tags" 
-}
-
 variable "instance_type" {
   type        = string
   description = "EC2 instance type"
@@ -23,6 +18,32 @@ variable "subnet_type" {
   description = "The type of subnet to deploy instance"
 }
 
+
+variable "cost_center" {
+  type        = string
+  description = "Cost Center to associate instance"
+}
+
+variable "instance_name" {
+  type        = string
+  description = "The name of the instance"
+  validation {
+    condition = can(regex("^[a-zA-Z]+$", var.instance_name)) && length(var.instance_name) == 6
+    error_message = "Instance name must be 6 characters in length containing only [a-zA-Z]"
+  }
+}
+variable "instance_profile" {
+  type        = string
+  description = "The name of the instance profile"
+  default     = "HF-EC2-SSMRole"
+}
+
+variable "user_data" {
+  type        = string
+  description = "User data to apply on instance"
+  default     = ""
+}
+
 variable "ami_filters" {
   type        = map
   description = "Global ami filters"
@@ -37,54 +58,3 @@ variable "cost_centers" {
   type        = map
   description = "Global cost centers"
 }
-
-variable "cost_center" {
-  type        = string
-  description = "Cost Center to associate instance"
-}
-
-#variable "ami" {
-#  type        = string
-#  description = "The description of the VM"
-#}
-
-#variable "subnet_ids" {
-#  type        = string
-#  description = "The list of subnet used by the instance"
-#}
-
-#variable "security_groups" {
-#  type        = list(string)
-#  description = "The list of security groups used by the instance"
-#}
-
-#variable "key_name" {
-#  type        = string
-#  description = "The key used to ssh into the instance"
-#}
-
-variable "instance_name" {
-  type        = string
-  description = "The name of the instance"
-  validation {
-    condition = can(regex("^[a-zA-Z]+$", var.instance_name)) && length(var.instance_name) == 6
-    error_message = "Instance name must be 6 characters in length containing only [a-zA-Z]"
-  }
-}
-
-variable "instance_profile" {
-  type        = string
-  description = "The name of the instance profile"
-  default     = "HF-EC2-SSMRole"
-}
-
-variable "user_data" {
-  type        = string
-  description = "User data to apply on instance"
-  default     = ""
-}
-
-#variable "bc_password" {
-#  type        = string
-#  description = "The Bluecat password"
-#}
